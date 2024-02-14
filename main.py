4cba8507cb75d966e09d5fe3f48e4c11260d43e7
Caleb Givens <69734345+PizzaGamer404@users.noreply.github.com>
2024-02-14 14:46:46 +0000

    Add misspelled variations of "your" and "you're" to check for terrible spelling in messages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # https://discordpy.readthedocs.io/en/latest/api.html
 import discord
+import openai
 
 # Opens discord_key.secret and reads the key
 with open('discord_key.secret', 'r') as f:
@@ -10,7 +11,20 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
 
-your_set = {"you're","your","youre","ur"}
+misspellings_your = {
+    "yur", "youre", "yor", "you'r", "ur", "yore", "youre", "yuor", "yoir", 
+    "youre'", "yhour", "your'e", "yur", "yours", "yuo", "yout", "yorr",
+    "yuur", "yoour", "yuer", "yours", "yourr", "yuore",
+    "yure", "yuor", "yhur", "youur", "yorr", "yoor", "yowr", "yuour", "yours","yoooour"
+}
+
+misspellings_youre = {
+    "your'e", "youre", "yuore", "yore", "you'r", "yure", "yo're",
+    "youre'", "yor'e", "yuo're", "y'oure", "youre", "yu're", "u're", "yur'e",
+    "yourre", "your'e", "yorue", "yuoure", "yuo're", "you'are", "youre", "your'e",
+    "yow're", "you'er", "yure'", "yuor'e", "yoru'e", "youer"
+}
+all_yours = misspellings_your.union(misspellings_youre)
 
 @bot.event
 async def on_message(message: discord.Message):
@@ -18,9 +32,13 @@ async def on_message(message: discord.Message):
         return
 
     words = message.content.split()
+    terrible_spelling = False
     for word in words:
-        if word in your_set:
-            await message.reply('AAAAAAAAAAAAAAAAAAAA (╯°□°)╯︵ ┻━┻')
+        if word.lower() in all_yours:
+            terrible_spelling = True
+            break
+    if terrible_spelling == True:
+        await message.reply('AAAAAAAAAAAAAAAAAAAA (╯°□°)╯︵ ┻━┻')
 
 
 # Runs the bot
